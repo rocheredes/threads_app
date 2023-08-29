750ec41774a53cd98574ec6af29ec0ea4f6d9b87
Alejandro J Roche Benitez <rochebenitez90@gmail.com>
2023-08-29 19:32:27 -0400

    Topbar components with clerck organization

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,3 +1,4 @@
+import { OrganizationSwitcher, SignOutButton, SignedIn } from '@clerk/nextjs'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
@@ -16,6 +17,36 @@ const Topbar = () => {
                     Threads
                 </p>
             </Link>
+
+            <div className='flex items-center gap-1'>
+                <div className='block md:hidden'>
+                    <SignedIn>
+                        <SignOutButton >
+                            <div className='flex cursor-pointer'>
+                                <Image
+                                    src="/assets/logout.svg"
+                                    alt="logout"
+                                    width={24}
+                                    height={24}
+                                />
+                            </div>
+                        </SignOutButton>
+                    </SignedIn>
+                </div>
+
+        <div>
+            <OrganizationSwitcher 
+                appearance={{
+                    elements:{
+                        organizationSwitcherTrigger:"py-2 px-4"
+                    }
+                }}
+            />
+        </div>
+
+
+            </div>
+
         </nav>
     )
 }
