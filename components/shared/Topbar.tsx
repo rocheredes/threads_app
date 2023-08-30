6bb81dd65ab2ec18df2badbb0e87113a74f31a7b
Alejandro J Roche Benitez <rochebenitez90@gmail.com>
2023-08-29 23:17:21 -0400

    add darkTheme to clerk

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,6 +1,7 @@
 import { OrganizationSwitcher, SignOutButton, SignedIn } from '@clerk/nextjs'
 import Image from 'next/image'
 import Link from 'next/link'
+import { dark } from '@clerk/themes'
 import React from 'react'
 
 const Topbar = () => {
@@ -34,15 +35,16 @@ const Topbar = () => {
                     </SignedIn>
                 </div>
 
-        <div>
-            <OrganizationSwitcher 
-                appearance={{
-                    elements:{
-                        organizationSwitcherTrigger:"py-2 px-4"
-                    }
-                }}
-            />
-        </div>
+                <div>
+                    <OrganizationSwitcher
+                        appearance={{
+                            baseTheme: dark,
+                            elements: {
+                                organizationSwitcherTrigger: "py-2 px-4"
+                            }
+                        }}
+                    />
+                </div>
 
 
             </div>
