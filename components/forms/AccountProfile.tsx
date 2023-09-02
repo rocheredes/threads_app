8c915b2703656fc031eda0cbd6fcad6230016be0
Alejandro J Roche Benitez <rochebenitez90@gmail.com>
2023-09-01 22:21:58 -0400

    onSubmit method

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -1,6 +1,7 @@
 'use client'
-import React, { ChangeEvent } from 'react'
-
+import React, { ChangeEvent, useState } from 'react'
+import Image from 'next/image'
+import * as z from "zod"
 import { useForm } from 'react-hook-form'
 import { zodResolver } from "@hookform/resolvers/zod"
 import { UserValidation } from '@/lib/validations/user'
@@ -16,9 +17,9 @@ import {
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { Textarea } from '../ui/textarea'
-import * as z from "zod"
-import Image from 'next/image'
-
+import { url } from 'inspector'
+import { isBase64Image } from '@/lib/utils'
+import { useUploadThing } from '@/lib/uploadthing'
 
 interface Props {
     user: {
@@ -34,25 +35,56 @@ interface Props {
 
 
 const AccountProfile = ({ user, btnTitle }: Props) => {
+
+    const [files, setFiles] = useState<File[]>([])
+    const { startUpload } = useUploadThing("media")
+
     const form = useForm({
         resolver: zodResolver(UserValidation),
         defaultValues: {
-            profile_photo: user?.image || "",
-            name: user?.name || "",
-            username: user?.username || "",
-            bio: user?.bio || ""
-        }
+            profile_photo: user?.image ? user.image : "",
+            name: user?.name ? user.name : "",
+            username: user?.username ? user.username : "",
+            bio: user?.bio ? user.bio : "",
+        },
     })
 
 
-    function onSubmit(values: z.infer<typeof UserValidation>) {
-        // Do something with the form values.
-        // checkL This will be type-safe and validated.
-        console.log(values)
+    const onSubmit = async (values: z.infer<typeof UserValidation>) {
+        const blob = values.profile_photo
+
+        const hasImageChanged = isBase64Image(blob)
+        if (hasImageChanged) {
+            const imgRes = await startUpload(files)
+
+            if (imgRes && imgRes[0].url) {
+                values.profile_photo = imgRes[0].url
+            }
+        }
+
+        //Todo: Update user profile
+
     }
 
-    const handleImage = (e: ChangeEvent, fieldChange: (value: string) => void) => {
+    const handleImage = (e: ChangeEvent<HTMLInputElement>, fieldChange: (value: string) => void) => {
         e.preventDefault()
+        const fileReader = new FileReader()
+        if (e.target.files && e.target.files.length > 0) {
+            const file = e.target.files[0]
+            setFiles(Array.from(e.target.files))
+
+            if (!file.type.includes('image')) return
+
+            fileReader.onload = async (event) => {
+                const imageDataUrl = event.target?.result?.toString() || ''
+                fieldChange(imageDataUrl)
+            }
+
+            fileReader.readAsDataURL(file)
+
+        }
+
+
     }
 
     return (
