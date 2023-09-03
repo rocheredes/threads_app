731ca7802443bd75bd146f4db4b109212b31530c
Alejandro J Roche Benitez <rochebenitez90@gmail.com>
2023-09-02 23:29:42 -0400

    thrads actions  and import thread page

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -3,6 +3,7 @@ import { revalidatePath } from "next/cache"
 import Thread from "../models/thread.model"
 import User from "../models/user.model"
 import { connectToDB } from "../mongoose"
+import { skip } from "node:test"
 
 interface Params {
     text: string,
@@ -30,5 +31,34 @@ export async function createThread({ text, author, communityId, path }: Params) 
     }
 
 
+
+}
+
+export async function fetchPosts(pageNumber = 1, pageSize = 20) {
+
+    connectToDB()
+
+    const skipAmount = (pageNumber - 1) * pageSize
+
+    // Fetch the post that have no parents (top level thread)
+    const postQuery = Thread.find({ parentId: { $in: [null, undefined] } })
+        .sort({ createdAt: 'desc' })
+        .skip(skipAmount)
+        .limit(pageSize)
+        .populate({ path: 'author', model: User })
+        .populate({
+            path: 'children',
+            populate: {
+                path: 'author',
+                model: User,
+                select: "_id name parentId image"
+            }
+        })
+
+    const totalPostCount = await Thread.countDocuments({ parentId: { $in: [null, undefined] } })
+    const posts = await postQuery.exec()
+    const isNext = totalPostCount > skipAmount + posts.length
+
+    return { posts, isNext }
 
 }
