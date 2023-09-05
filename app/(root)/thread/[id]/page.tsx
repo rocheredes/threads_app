9f79a19f3edefb39f20f6b954206c9cca0f8efb1
Alejandro J Roche Benitez <rochebenitez90@gmail.com>
2023-09-05 18:58:47 -0400

    show thread card

--- app/(root)/thread/[id]/page.tsx
+++ app/(root)/thread/[id]/page.tsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import ThreadCard from '@/components/cards/ThreadCard'
+import { fetchThreadById } from '@/lib/actions/thread.actions'
+import { fetchUser } from '@/lib/actions/user.actions'
+import { currentUser } from '@clerk/nextjs'
+import { redirect } from 'next/navigation'
 
-const ThreadDetailPage = () => {
-  return (
-    <div>ThreadDetailPage</div>
-  )
+
+
+const ThreadDetailPage = async ({ params }: { params: { id: string } }) => {
+    if (!params.id) return null
+    const user = await currentUser()
+
+    if (!user) return null
+    const userInfo = await fetchUser(user.id)
+
+    if (!userInfo?.onboarded) return redirect('/onboarding')
+
+    const thread = await fetchThreadById(params.id)
+
+    return (
+        <section className='relative'>
+            <div>
+                <ThreadCard
+                    key={thread._id}
+                    id={thread._id}
+                    currentUserId={user?.id || ''}
+                    parentId={thread.parentId}
+                    content={thread.text}
+                    author={thread.author}
+                    comunnity={thread.community}
+                    createdAt={thread.createdAt}
+                    comments={thread.comments}
+
+                />
+            </div>
+        </section>
+    )
 }
 
 export default ThreadDetailPage
