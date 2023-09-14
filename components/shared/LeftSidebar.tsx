2f4f830179ba37c7ab3adf261a36c714c4d3ae9f
Alejandro J Roche Benitez <rochebenitez90@gmail.com>
2023-09-14 18:00:23 -0400

    fix minor bugs

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -4,17 +4,21 @@ import React from 'react'
 import { sidebarLinks } from '@/constants/index'
 import Image from 'next/image'
 import { useRouter, usePathname } from 'next/navigation'
-import { SignOutButton, SignedIn } from '@clerk/nextjs'
+import { SignOutButton, SignedIn, useAuth } from '@clerk/nextjs'
 
 const LeftSidebar = () => {
   const router = useRouter()
   const pathname = usePathname()
+  const { userId } = useAuth()
   return (
     <section className='leftsidebar custom-scrollbar'>
       <div className='flex w-full flex-1 flex-col gap-6 px-6'>
         {
           sidebarLinks.map((link) => {
             const isActive = (pathname.includes(link.route) && link.route.length > 1) || pathname === link.route
+
+            if (link.route === '/profile') link.route = `${link.route}/${userId}`
+
             return (
               <Link
                 href={link.route}
@@ -39,7 +43,7 @@ const LeftSidebar = () => {
 
       <div className='mt-10 px-6'>
         <SignedIn>
-          <SignOutButton signOutCallback={()=>router.push('/sign-in')}>
+          <SignOutButton signOutCallback={() => router.push('/sign-in')}>
             <div className='flex cursor-pointer gap-4 px-4'>
               <Image
                 src="/assets/logout.svg"
