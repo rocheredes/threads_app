2ae4833c4bb4c2049fcab7a7ef03b5ab68fcb85e
Alejandro J Roche Benitez <rochebenitez90@gmail.com>
2023-09-14 20:14:07 -0400

    fetch user action

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -1,9 +1,27 @@
+import PostThread from "@/components/forms/PostThread"
+import ProfileHeader from "@/components/shared/ProfileHeader"
+import ThreadsTab from "@/components/shared/ThreadsTab"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { profileTabs } from "@/constants"
+import { fetchUser } from "@/lib/actions/user.actions"
+import { currentUser } from "@clerk/nextjs"
+import Image from "next/image"
+import { redirect } from 'next/navigation'
 
 
 const SearchPage = async () => {
+  const user = await currentUser()
+
+  if (!user) return null
+
+  const userInfo = await fetchUser(user.id)
+
+  // Fetch Users
+
+  if (!userInfo?.onboarded) redirect('/onboarding')
   return (
     <section>
-        <h1 className="head-text mb-10">Search</h1>
+      <h1 className="head-text mb-10">Search</h1>
     </section>
   )
 }
