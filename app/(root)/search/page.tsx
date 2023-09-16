12a919b830febe33e91a1df6c83903c81defbd67
Alejandro J Roche Benitez <rochebenitez90@gmail.com>
2023-09-16 15:39:40 -0400

    activities page

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -1,12 +1,6 @@
 import UserCard from "@/components/cards/UserCard"
-import PostThread from "@/components/forms/PostThread"
-import ProfileHeader from "@/components/shared/ProfileHeader"
-import ThreadsTab from "@/components/shared/ThreadsTab"
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
-import { profileTabs } from "@/constants"
 import { fetchUser, fetchUsers } from "@/lib/actions/user.actions"
 import { currentUser } from "@clerk/nextjs"
-import Image from "next/image"
 import { redirect } from 'next/navigation'
 
 
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -137,4 +137,34 @@ export async function fetchUsers({
     } catch (error: any) {
         throw new Error(`Failed to fetch users: ${error.message}`)
     }
+
+
+}
+
+export async function getActivities(userId: string) {
+    try {
+        connectToDB()
+
+        const userThreads = await Thread.find({ author: userId })
+
+        //Collect all the child threads ids( replies ) from the childs
+        const childThreadIds = userThreads.reduce((acc, userThread) => {
+            return acc.concat(userThread.children)
+        },[])
+
+        const replies = await Thread.find({
+            _id: { $in: childThreadIds },
+            author: { $ne: userId }
+        }).populate({
+            path: 'author',
+            model: User,
+            select: 'name image _id'
+        })
+
+        return replies
+
+
+    } catch (error: any) {
+        throw new Error(`Faildes to fetch activity: ${error.message}`)
+    }
 }
